168b33b167386a537d513b1d6c40fa67aad4fc67
Liu <clara.zirui@gmail.com>
2023-07-25 14:24:30 +0100

    initial commit

--- app.py
+++ app.py
@@ -4,20 +4,38 @@ from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 import matplotlib.pyplot as plt
 
+
+def gen_prompt(prompt):
+    if 'plot' in prompt.lower():
+        prompt = f"""
+        {prompt}.
+        Show the plot, and return the data in the following
+        JSON format: {{"x": , "y": , "plot_type":, "plot_title": }}
+        """
+    return prompt
+
 st.title("pandas-ai streamlit interface")
 
 st.write("A demo interface for [PandasAI](https://github.com/gventuri/pandas-ai)")
-st.write(
-    "Looking for an example *.csv-file?, check [here](https://gist.github.com/netj/8836201)."
-)
 
 if "openai_key" not in st.session_state:
     with st.form("API key"):
         key = st.text_input("OpenAI Key", value="", type="password")
         if st.form_submit_button("Submit"):
+            if not len(key):
+                key = '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'
             st.session_state.openai_key = key
             st.session_state.prompt_history = []
             st.session_state.df = None
+
+examples = f"""
+Here are some example queries:
+- Plot the number of customer by country in a pie chart
+- Plot the mean total over time for Germany
+- How many customers are there in Oslo
+"""
+with st.sidebar:
+    st.markdown(examples)
 
 if "openai_key" in st.session_state:
     if st.session_state.df is None:
@@ -28,7 +46,9 @@ if "openai_key" in st.session_state:
         if uploaded_file is not None:
             df = pd.read_csv(uploaded_file)
             st.session_state.df = df
-
+    if st.session_state.df is not None:
+        st.subheader("Current dataframe:")
+        st.write(st.session_state.df)
     with st.form("Question"):
         question = st.text_input("Question", value="", type="default")
         submitted = st.form_submit_button("Submit")
@@ -36,17 +56,12 @@ if "openai_key" in st.session_state:
             with st.spinner():
                 llm = OpenAI(api_token=st.session_state.openai_key)
                 pandas_ai = PandasAI(llm)
-                x = pandas_ai.run(st.session_state.df, prompt=question)
-
+                x = pandas_ai(st.session_state.df, prompt=gen_prompt(question))
                 fig = plt.gcf()
                 if fig.get_axes():
                     st.pyplot(fig)
                 st.write(x)
                 st.session_state.prompt_history.append(question)
-
-    if st.session_state.df is not None:
-        st.subheader("Current dataframe:")
-        st.write(st.session_state.df)
 
     st.subheader("Prompt history:")
     st.write(st.session_state.prompt_history)
